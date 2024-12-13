6e0dd4fa0e581caa8b93344254754ede2054cc29
Nurgül Yalman <123768429+nurshahs@users.noreply.github.com>
2024-12-13 19:03:41 +0300

    Add files via upload

---
+++ hw8.js
@@ -0,0 +1,17 @@
+const age = 18;
+if (age >= 18) {
+    console.log("You are eligible to vote.");
+} else {
+    console.log("You are not eligible to vote.");
+}
+
+// 2. Example: Mathematical operation using operators
+const a = 10;
+const b = 5;
+const result = a * b + (a - b);
+console.log("The result of the mathematical operation is:", result);
+
+// 3. Example: For loop to print numbers from 1 to 10
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
